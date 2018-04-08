3a7a4f65b8f12b5ffd9da6e1565ba69c0f536af7
keli <keli@bigbaser.com>
2018-04-08 18:47:15 +0800

    db

--- pro/routes/users.js
+++ pro/routes/users.js
@@ -1,13 +1,13 @@
 import express  from 'express'
-import {HandleDB} from '../dao/db'
+import {db} from '../dao/db'
 let router = express.Router();
-let db = new HandleDB();
 /* GET users listing. */
-router.post('/signUp', function (req, res, next) {
-  db.sql(
-    `insert into users (user_name,password,email,gender,avatar,phone,job_no) 
+router.post('/signUp', async function (req, res, next) {
+  await db(
+    'all',
+    `insert into users(user_name,password,email,gender,avatar,phone,job_no) 
     values(?,?,?,?,?,?,?)`,
-    ['keli', '123456', '670550188@qq.com', 1, 'http://baidu.com', 13233333333, 4],
+    ['keli', '123456', '670550188@qq.com', 1, 'http://baidu.com', '13233333333', 4],
     'all'
   ).then(resp=>{
     res.json({a:32})
